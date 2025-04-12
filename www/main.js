e1ac0e2fc57b3e1a0d1262a6a1732dbed09370a5
Nguyễn Văn A <vana@gmail.com>
2025-04-12 19:32:33 +0700

    new stage

--- www/main.js
+++ www/main.js
@@ -9,4 +9,27 @@ $(document).ready(function () {
       effect: "bounceOut",
     },
   });
+  // Siri configuration
+  var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 840,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.30",
+    autostart: true,
+  });
+  // siri message animation
+  $(".siri-message").textillate({
+    loop: true,
+    sync: true,
+    in: {
+      effect: "bounceIn",
+      sync: true,
+    },
+    out: {
+      effect: "bounceOut",
+      sync: true,
+    },
+  });
 });
