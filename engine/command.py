ae98196a44f882bea2d30edaaf79a4a8c3e178f6
Nguyễn Văn A <vana@gmail.com>
2025-04-15 18:43:13 +0700

    model

--- engine/command.py
+++ engine/command.py
@@ -1,11 +1,30 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)  # Change index to select different voice
+    engine.setProperty('voice', voices[0].id)  # Change index to select different voice
     engine.setProperty('rate', 150)  # Speed of speech
+    print(voices)
     engine.say(text)
     engine.runAndWait()
+def takecommand():
+    r = sr.Recognizer()
 
-speak("I love Vietnam")
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing....')
+        query = r.recognize_google(audio, language='vi-VN ')
+        print(f"user said: {query}\n")
+    except Exception as e:
+        return ""
+    return query.lower()
+text = takecommand()
+speak(text)
